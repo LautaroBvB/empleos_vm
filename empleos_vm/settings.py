4617a128a675c8daa5c5613bdeb2c167c1fcbaa0
Lautaro <lautarozarate826@hotmail.com>
2025-02-25 15:44:39 -0300

    debug true

--- empleos_vm/settings.py
+++ empleos_vm/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$*xtrdry23%%c+s66+to0p(q0)i7$yj*1j&*)b%30tj9kh1(w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
