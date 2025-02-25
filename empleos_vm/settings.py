b93588f9d27166aae09a7ff3a8af7cbd721ca2fc
Lautaro <lautarozarate826@hotmail.com>
2025-02-25 15:41:40 -0300

    lanzando app

--- empleos_vm/settings.py
+++ empleos_vm/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
