619b80369d022eefe5c20dbce6496d975ac06fc5
Lautaro <lautarozarate826@hotmail.com>
2025-03-03 00:44:41 -0300

    modificaciones en settings.py

--- aplicacionmobile/settings.py
+++ aplicacionmobile/settings.py
@@ -34,6 +34,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
